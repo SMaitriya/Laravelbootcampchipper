0ab57e11d167241a79f824c650acc274bedcc2f9
Maitriya Sramaner <maitriya@outlook.fr>
2024-02-07 19:35:55 +0100

    added delete function

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,7 @@ Route::middleware('auth')->group(function () {
 
 
 Route::resource('chirps', ChirpController::class)
-    ->only(['index', 'store', 'update'])
+    ->only(['index', 'store', 'update', 'destroy'])
     ->middleware(['auth', 'verified']);
 
     Route::get('retour', [ChirpController::class, 'try'])->name('retour')->middleware(['auth', 'verified']);
